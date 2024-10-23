8f0cd8df8552821882103c2c62db78b60d6c71fc
anibal <anibaloso@live.com>
2024-10-23 11:52:21 -0300

    wip Creacion de footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import NavBarComponent from './components/NavBarComponent.vue';
-
+import FooterComponent from '@/components/FooterComponent.vue'
 </script>
 
 <template>
-  <NavBarComponent />
-  <RouterView />
+  <div class="min-vh-100 d-flex flex-column">
+    <NavBarComponent />
+    <main class="flex-fill py-2">
+      <RouterView />
+    </main>
+    <FooterComponent />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 text-center ">
+                <h5>Redes Sociales</h5>
+
+                <div class="row " id="redes">
+                    <div class="col-6 text-center">
+                        <a href="https://www.instagram.com/marizinhaboutique/"><i
+                                class="fa-brands fa-instagram me-5"></i></a>
+                        <a href="https://www.facebook.com/marizinharoupas"><i
+                                class="fa-brands fa-square-facebook ms-5"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 text-center ">
+                <h5>Contacto</h5>
+                <div class="row " id="contacto">
+                    <div class="col-6">
+                        <i class="fa-solid fa-phone"></i>
+
+                        <p>(+56) 9 8765 4321</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+<style scoped>
+.container {
+    margin-top: 2rem;
+}
+
+.fa-solid {
+    align-items: center;
+}
+
+#contacto {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+i {
+    font-size: xx-large;
+}
+
+.fa-phone {
+    font-size: large;
+
+}
+
+#redes {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
